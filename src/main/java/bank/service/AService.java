992b3b778662e6a4c6387aac76cb30287c5c9472
Piotr Marecki <pmarecki.pro@gmail.com>
2017-06-17 12:27:57 +0200

    Add AssertJ testing framework

---
+++ src/main/java/bank/service/AService.java
@@ -0,0 +1,17 @@
+package bank.service;
+
+public class AService {
+
+
+    int findIndex(int[] a, int x) {
+        //// write it
+        return 0;
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+
+
+}
---
+++ src/test/bank/service/AServiceTest.java
@@ -0,0 +1,33 @@
+package bank.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
+
+public class AServiceTest {
+    AService testee;
+
+
+    @Before
+    public void setUp() throws Exception {
+        testee = new AService();
+    }
+
+    @Test
+    public void aaaTest() {
+        assertThat(1).isEqualTo(1);
+    }
+
+    @Test
+    public void simpleFindTest() {
+        int[] a = new int[]{0, 2, 8, 15, 16};
+
+        assertThat(testee.findIndex(a, 5)).isEqualTo(2);
+        assertThat(testee.findIndex(a, 8)).isEqualTo(2);
+        assertThat(testee.findIndex(a, 20)).isEqualTo(4);
+        assertThat(testee.findIndex(a, -20)).isEqualTo(0);
+        assertThat(testee.findIndex(a, 0)).isEqualTo(0);
+    }
+}
